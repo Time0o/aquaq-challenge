99b571325c9297dc74db007376e1c984487258e5
Timo Nicolai <timonicolai@arcor.de>
2019-03-31 17:24:50 +0200

    Add rose_by_any_other_name.cc

---
+++ rose_by_any_other_name.cc
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+int main()
+{
+    std::string input;
+    if (!getline(std::cin, input) && std::cin.bad()) {
+        std::cerr << "failed to read input\n";
+        exit(EXIT_FAILURE);
+    }
+
+    for (char &c : input) {
+        if (!isxdigit(c))
+            c = '0';
+    }
+
+    input += std::string(3 - input.length() % 3, '0');
+    auto chunk_size = input.length() / 3;
+
+    std::stringstream ss;
+    for (int i = 0; i < 3; ++i)
+        ss << input[i * chunk_size] << input[i * chunk_size + 1];
+
+    std::cout << ss.str() << '\n';
+
+    exit(EXIT_SUCCESS);
+}
