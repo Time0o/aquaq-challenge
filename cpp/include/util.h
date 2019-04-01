e3cf8d0e30a7bea9b598e376bbe90abc428dd68e
Timo Nicolai <timonicolai@arcor.de>
2019-04-01 18:55:23 +0200

    Move csv reading to include/util.h

---
+++ cpp/include/util.h
@@ -0,0 +1,73 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+std::vector<std::string> split(std::string const &str, char delim=',')
+{
+    std::vector<std::string> res;
+
+    std::stringstream ss(str);
+    std::string token;
+
+    while (ss.good()) {
+        std::getline(ss, token, delim);
+        res.push_back(token);
+    }
+
+    return res;
+}
+
+
+std::vector<std::vector<std::string>> read_csv(
+    std::istream &is, std::vector<std::string> const &columns, char delim=',')
+{
+    std::vector<decltype(columns.size())> column_indices(columns.size());
+    std::vector<std::vector<std::string>> csv;
+
+    // read loop
+    std::string line;
+    bool first = true;
+
+    while (std::getline(is, line)) {
+        std::vector<std::string> tokens(split(line, delim));
+        assert(tokens.size() == columns.size());
+
+        if (first) {
+            // validate column names and determine column order
+            std::set<std::string> columns_actual;
+
+            for (auto i = 0u; i < 3u; ++i) {
+                columns_actual.insert(tokens[i]);
+
+                auto column_index = std::find(
+                    columns.begin(), columns.end(), tokens[i]) - columns.begin();
+
+                column_indices[i] = column_index;
+            }
+
+            assert(columns_actual ==
+                   std::set<std::string>(columns.begin(), columns.end()));
+
+            first = false;
+        } else {
+            // append row
+            std::vector<std::string> row(columns.size());
+
+            for (auto i = 0u; i < row.size(); ++i)
+                row[column_indices[i]] = tokens[i];
+
+            csv.push_back(row);
+        }
+    }
+
+    return csv;
+}
+
+#endif // UTIL_H
--- cpp/src/what_is_best_in_life.cc
+++ cpp/src/what_is_best_in_life.cc
@@ -6,10 +6,11 @@
 #include <map>
 #include <regex>
 #include <set>
-#include <sstream>
 #include <string>
 #include <utility>
 #include <vector>
+
+#include "util.h"
 
 
 enum { INITIAL_elo = 1200 };
@@ -32,85 +33,44 @@ static void update_elo(double &elo_winner, double &elo_loser)
 }
 
 
-static std::vector<std::string> split(std::string const &str, char delim=',')
-{
-    std::vector<std::string> res;
-
-    std::stringstream ss(str);
-    std::string token;
-
-    while (ss.good()) {
-        std::getline(ss, token, delim);
-        res.push_back(token);
-    }
-
-    return res;
-}
-
-
 int main()
 {
     // intermediate elo scores of all players
     std::map<std::string, double> elos;
 
-    // CSV column index mapping
-    std::map<std::string, std::vector<std::string>::size_type> column_ind;
+    // read match results and update elo scores
+    auto csv = read_csv(std::cin, {"h", "a", "score"});
 
-    std::string line;
-    bool first;
-    while (std::getline(std::cin, line)) {
-        // tokenize line
-        std::vector<std::string> tokens(split(line));
-        assert(tokens.size() == 3u);
+    for (auto const &row : csv) {
+        std::string h(row[0]);
+        std::string a(row[1]);
 
-        if (first) {
-            // determine column index mapping
-            std::set<std::string> columns;
-            for (auto i = 0u; i < 3u; ++i) {
-                columns.insert(tokens[i]);
-                column_ind[tokens[i]] = i;
-            }
-
-            // validate column identifiers
-            assert(columns == std::set<std::string>({"h", "a", "score"}));
-
-            first = false;
-        } else {
-            std::string h(tokens[column_ind["h"]]);
-            std::string a(tokens[column_ind["a"]]);
-
-            // set initial elo for newly encountered players
-            for (auto const &player : {h, a}) {
-                if (elos.find(player) == elos.end())
-                    elos[player] = INITIAL_elo;
-            }
-
-            // parse score
-            std::string score(tokens[column_ind["score"]]);
-
-            std::cmatch cm;
-            if (!std::regex_match(score.c_str(), cm, score_reg)) {
-                std::cerr << "failed to parse '" << score << "'\n";
-                exit(EXIT_FAILURE);
-            }
-
-            // update elo
-            double elo_h = elos[h];
-            double elo_a = elos[a];
-
-            if (std::stoi(cm[1]) > std::stoi(cm[2]))
-                update_elo(elo_h, elo_a);
-            else
-                update_elo(elo_a, elo_h);
-
-            elos[h] = elo_h;
-            elos[a] = elo_a;
+        // set initial elo for newly encountered players
+        for (auto const &player : {h, a}) {
+            if (elos.find(player) == elos.end())
+                elos[player] = INITIAL_elo;
         }
-    }
 
-    if (!first) {
-        std::cerr << "missing input\n";
-        exit(EXIT_FAILURE);
+        // parse score
+        std::string score(row[2]);
+
+        std::cmatch cm;
+        if (!std::regex_match(score.c_str(), cm, score_reg)) {
+            std::cerr << "failed to parse '" << score << "'\n";
+            exit(EXIT_FAILURE);
+        }
+
+        // update elo
+        double elo_h = elos[h];
+        double elo_a = elos[a];
+
+        if (std::stoi(cm[1]) > std::stoi(cm[2]))
+            update_elo(elo_h, elo_a);
+        else
+            update_elo(elo_a, elo_h);
+
+        elos[h] = elo_h;
+        elos[a] = elo_a;
     }
 
     if (std::cin.bad()) {
